b9b52e07c0004bc87faf08bbaa4a49f5f23e522d
Vignesh <kkvignesh2000@gmail.com>
2024-07-05 08:52:15 +0530

    added cros-origin-resource policy header

--- vite.config.ts
+++ vite.config.ts
@@ -21,6 +21,7 @@ export default defineConfig({
     headers: {
       "Cross-Origin-Embedder-Policy": "require-corp",
       "Cross-Origin-Opener-Policy": "same-origin",
+      "Cross-Origin-Resource-Policy": "cross-origin",
     },
   },
 });
